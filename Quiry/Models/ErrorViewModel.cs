a0c6021129272b54f2b0f59aef837246f95ecb93
KBonnicksen <kelseybon@gmail.com>
2019-09-25 18:14:56 -0700

    Add project files.

---
+++ Quiry/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quiry.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
