b9c2e554aae8999c641b0924abc81008a20838e3
KBonnicksen <kelseybon@gmail.com>
2019-09-25 21:17:53 -0700

    Created template for contact page

--- Quiry/Controllers/HomeController.cs
+++ Quiry/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace Quiry.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
